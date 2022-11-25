a0d4fea9eaeab36bf525a5acd13cd56d6cf78b64
John <John01166@ya.ru>
2022-11-25 16:46:33 +0300

    program

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+void FillArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+            Console.Write($"{array[i]} ");
+    }
+}
+
+string[] ex1 = {"1", "22", "333", "4444", "333"};
+string[] ex2 = new string[ex1.Length];
+
+FillArray (ex1, ex2);
+PrintArray(ex2);
